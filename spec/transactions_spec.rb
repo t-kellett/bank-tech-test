a34d816e9f3f745035dda9a2b1168e3074e4b8ef
Tom <t.p.kellett@gmail.com>
2021-11-22 14:41:08 +0000

    refactoring transaction tests

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -1,15 +1,20 @@
 require 'transactions'
 
 describe Transactions do
+  let(:account) { described_class.new }
+
+  before do
+    account.add_transaction(credit: 500.00, balance: 500.00)
+    account.add_transaction(debit: 300.00, balance: 200.00)
+  end
+
   context "#add_transaction" do
     it "adds a credit transaction to the transactions history array" do
-      subject.add_transaction(credit: 500.00, balance: 500.00)
-      expect(subject.history.last).to eq({:credit => 500.00, :debit => "", :balance => 500.00 })
+      expect(account.history.first).to eq({:credit => 500.00, :debit => "", :balance => 500.00 })
     end
 
     it "adds a debit transaction to the transactions history array" do
-      subject.add_transaction(debit: 300.00, balance: 200.00)
-      expect(subject.history.last).to eq({:credit => "", :debit => 300.00, :balance => 200.00})
+      expect(account.history.last).to eq({:credit => "", :debit => 300.00, :balance => 200.00})
     end
   end
 end
