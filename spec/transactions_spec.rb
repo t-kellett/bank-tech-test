b4847bed49ba723f1cb1c3b4e8fc18547ed62f0c
Tom <t.p.kellett@gmail.com>
2021-11-22 14:35:45 +0000

    adding a passing unit test adding a debit transaction

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -4,7 +4,12 @@ describe Transactions do
   context "#add_transaction" do
     it "adds a credit transaction to the transactions history array" do
       subject.add_transaction(credit: 500.00, balance: 500.00)
-      expect(subject.history.first).to eq({:credit => 500.00, :debit => "", :balance => 500.00 })
+      expect(subject.history.last).to eq({:credit => 500.00, :debit => "", :balance => 500.00 })
+    end
+
+    it "adds a debit transaction to the transactions history array" do
+      subject.add_transaction(debit: 300.00, balance: 200.00)
+      expect(subject.history.last).to eq({:credit => "", :debit => 300.00, :balance => 200.00})
     end
   end
 end
