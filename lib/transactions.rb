63c86256054ce2ac96bc079b06b3a568e13f88ff
Tom <t.p.kellett@gmail.com>
2021-11-22 16:06:56 +0000

    adding passing unit test for print_statement

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Transactions
   attr_reader :history
 
@@ -5,7 +7,14 @@ class Transactions
     @history = []
   end
 
-  def add_transaction(credit: "", debit: "", balance: 0)
-    @history.push({:credit => credit, :debit => debit, :balance => balance})
+  def add_transaction(date: Date.today.strftime("%d/%m/%Y"), credit: "", debit: "", balance: "")
+    @history.push({:date => date, :credit => credit, :debit => debit, :balance => balance})
+  end
+
+  def print_statement
+    puts "date || credit || debit || balance"
+    @history.each do |item|
+      puts "#{item[:date]} || #{item[:credit]} || #{item[:debit]} || #{item[:balance]}".gsub!("  ", " ")
+    end
   end
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -1,20 +1,34 @@
 require 'transactions'
+require 'date'
 
 describe Transactions do
   let(:account) { described_class.new }
 
   before do
-    account.add_transaction(credit: 500.00, balance: 500.00)
-    account.add_transaction(debit: 300.00, balance: 200.00)
+    account.add_transaction(date: '22/11/2021', credit: "500.00", balance: "500.00")
+    account.add_transaction(date: '23/11/2021', debit: "300.00", balance: "200.00")
   end
 
   context "#add_transaction" do
     it "adds a credit transaction to the transactions history array" do
-      expect(account.history.first).to eq({:credit => 500.00, :debit => "", :balance => 500.00 })
+      expect(account.history.first).to eq({:date => '22/11/2021', :credit => "500.00", :debit => "", :balance => "500.00" })
     end
 
     it "adds a debit transaction to the transactions history array" do
-      expect(account.history.last).to eq({:credit => "", :debit => 300.00, :balance => 200.00})
+      expect(account.history.last).to eq({:date => '23/11/2021', :credit => "", :debit => "300.00", :balance => "200.00"})
+    end
+
+    it "adds today's date aand empty strings as default values if a date is not passed in" do
+      d = Date.today.strftime("%d/%m/%Y")
+      subject.add_transaction()
+      expect(subject.history.first).to eq({:date => d, :credit => "", :debit => "", :balance => ""})
+    end
+  end
+
+  context "#print_statement" do
+    it "prints the account statement to the console successfully" do
+      output = account.print_statement
+      expect(output).to eq(account.history)
     end
   end
 end
