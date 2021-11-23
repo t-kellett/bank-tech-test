8913587d52e882a820d40e21bc08231d76fc5180
Tom <t.p.kellett@gmail.com>
2021-11-23 15:30:30 +0000

    adding format statement method of new statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,2 +1,13 @@
 class Statement
+  attr_reader :transactions
+  
+  def initialize
+    @transactions = "date || credit || debit || balance"
+  end
+
+  def format_statement(transactions)
+    transactions.each do |item|
+      @transactions << "\n#{item[:date]} || #{item[:credit]} || #{item[:debit]} || #{item[:balance]}".gsub!("  ", " ")
+    end
+  end
 end
